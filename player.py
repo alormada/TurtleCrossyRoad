950f3624bcc42ffecd1eb7381f71b951e6b8256b
alormada <adam.rola110@gmail.com>
2025-03-02 16:59:38 +0100

    improved hitbox parameters handling

--- player.py
+++ player.py
@@ -6,10 +6,29 @@ class TurtlePlayer(Turtle):
         self.color("white")
         self.penup()
         self.shape("turtle")
+        self.shapesize(1.5)
         self.goto(0, -380)
         self.setheading(90)
+        self.y_hitbox = [self.ycor() - 15, self.ycor() + 25]
+        self.x_hitbox = [self.xcor() - 15, self.xcor() + 15]
+        self.set_hitbox()
 
     def move(self):
+        """
+        This function makes turtle move
+        :return:
+        """
         self.forward(20)
+        self.set_hitbox()
+
+    def go_at_start(self):
+        self.goto(0, -380)
+
+    def set_hitbox(self):
+        """
+        This function sets hitbox parameters of the turtle
+        :return:
+        """
+        self.y_hitbox = [self.ycor() - 15, self.ycor() + 25]
 
 
